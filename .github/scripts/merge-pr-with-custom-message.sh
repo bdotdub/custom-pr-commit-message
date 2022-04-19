fccd05d8a2bebb19da79635f7faeded5c5f44d2c
Benny Wong <benny@bwong.net>
2022-04-19 19:31:20 -0400

    Set git user

--- .github/scripts/merge-pr-with-custom-message.sh
+++ .github/scripts/merge-pr-with-custom-message.sh
@@ -1,3 +1,7 @@
+# First, set the git user
+git config --global user.name $(git show -s --format='%an' HEAD)
+git config --global user.email $(git show -s --format='%ae' HEAD)
+
 # Produce the squshed PR. By default, these changes are already staged.
 git merge --squash $SHA
 
