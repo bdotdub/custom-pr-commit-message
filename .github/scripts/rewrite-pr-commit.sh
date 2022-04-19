ad882044e0a27f38f6889c68da8b11e5b8a3f2f1
Benny Wong <benny@bwong.net>
2022-04-19 17:42:02 -0400

    Remove the need to set git user

--- .github/scripts/rewrite-pr-commit.sh
+++ .github/scripts/rewrite-pr-commit.sh
@@ -1,7 +1,3 @@
-# First, set the git user
-git config --global user.name $(git show -s --format='%an' HEAD)
-git config --global user.email $(git show -s --format='%ae' HEAD)
-
 # Pull
 git commit --amend -F - <<EOF
 $TITLE (#$NUMBER)
